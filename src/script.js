864f046238e64eaff1d55476612b45911e9da9f6
Demetrios <jkopatsis@gmail.com>
2023-03-23 21:34:48 -0700

    basis fetch complete, needs error handling

--- src/script.js
+++ src/script.js
@@ -1,2 +1,19 @@
 const key = require('./api.json');
-console.log(key.api);
+
+// console.log(key.api);
+
+async function fetchWeather(city){
+    const data = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=${key.api}&q=${city}`
+    );
+    const weather = await data.json();
+    return weather;
+}
+
+async function present(){
+    let laweather = await fetchWeather('los-angeles');
+    console.log(laweather.current);
+    console.log(laweather.location);
+}
+
+present();
