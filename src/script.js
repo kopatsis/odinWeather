fd52c0ce62b4bae54d657ada9fbc44f6c731bcff
Demetrios <jkopatsis@gmail.com>
2023-03-25 10:30:54 -0700

    unorthodox but handles error

--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
 const key = require('./api.json');
 
+const input = document.querySelector('#city');
+const search = document.querySelector('.search');
+const message = document.querySelector('.message');
+
 async function fetchWeather(city){
     const data = await fetch(
         `https://api.weatherapi.com/v1/current.json?key=${key.api}&q=${city}`
@@ -8,6 +12,21 @@ async function fetchWeather(city){
     return weather;
 }
 
-fetchWeather('sdajfajsdfas')
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
+search.addEventListener('click', mainWeather);
+
+async function mainWeather(){
+    try{
+        let cityname = input.value;
+        let cityweather = await fetchWeather(cityname);
+        console.log(cityweather);
+        if (cityweather.error != null){
+            message.textContent = "Failure"
+        } else{
+            message.textContent = `Success for ${cityweather.location.name}`
+        }    
+    } catch(error){
+        console.log(error);
+        message.textContent = "Failure"
+    }  
+}
+
