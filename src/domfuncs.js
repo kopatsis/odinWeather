d9ef33fa815dbd2ec5f1984dcca813c4c4b81978
Demetrios <jkopatsis@gmail.com>
2023-03-26 11:10:09 -0700

    fixed time, added date formatter

--- src/domfuncs.js
+++ src/domfuncs.js
@@ -15,16 +15,40 @@ function datatoDOM(weatherData){
     if (parseInt(weatherData.location.localtime.slice(11,13)) >= 13){
         let convertPM = parseInt(weatherData.location.localtime.slice(11,13)) - 12;
         document.querySelector('.time').textContent = "Local time: " + convertPM + weatherData.location.localtime.slice(13,16) + "pm";
+    } else if(parseInt(weatherData.location.localtime.slice(11,13)) === 12){
+        document.querySelector('.time').textContent = "Local time: " + weatherData.location.localtime.slice(11,16) + "pm";
+    } else if(parseInt(weatherData.location.localtime.slice(11,13)) === 0){
+        document.querySelector('.time').textContent = "Local time: " + "12" + weatherData.location.localtime.slice(12,15) + "am";
     } else {
         document.querySelector('.time').textContent = "Local time: " + weatherData.location.localtime.slice(11,16) + "am";
     }
     document.querySelector('.humid').textContent = `Humidity: ${weatherData.current.humidity}%`;
     document.querySelector('.condition').textContent = "Weather Condition: " + weatherData.current.condition.text;
+    document.querySelector('.date').textContent =   "Local Date: " +
+                                                    monthConv[parseInt(weatherData.location.localtime.slice(5,7))] +
+                                                    " " +
+                                                    weatherData.location.localtime.slice(8,10) +
+                                                    ", " +
+                                                    weatherData.location.localtime.slice(0,4);
+
 
     console.log(weatherData.location);
     console.log(weatherData.current);
 }
 
 const imperials = ["United States of America", "Liberia", "Myanmar"]
+const monthConv = { 1: "January", 
+                    2: "February", 
+                    3: "March", 
+                    4: "April", 
+                    5: "May", 
+                    6: "June", 
+                    7: "July", 
+                    8: "August", 
+                    9: "September",
+                    10: "October",
+                    11: "November",
+                    12: "December"
+                }
 
 export {datatoDOM, imperials}
